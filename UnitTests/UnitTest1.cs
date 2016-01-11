def7c6eb77b28e8b2755b11e077841e4fdd34825
tycho <work.tycho+git@gmail.com>
2016-01-11 12:08:58 +0000

    First Test Pass

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using didactic_palm_tree.UIModel;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ResistorTest
+    {
+        [TestMethod]
+        public void ResistorExists()
+        {
+            Resistor test = new Resistor();
+        }
+    }
+}
---
+++ didactic-palm-tree/UIModel/Resistor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace didactic_palm_tree.UIModel
+{
+    public class Resistor
+    {
+    }
+}
