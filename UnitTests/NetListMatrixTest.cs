dfebace98be864a37703dc7feceb501d9b817a4b
BuildTools <glsm.channel@gmail.com>
2016-01-15 10:25:35 +0000

    Added StarMath library reference

--- UnitTests/NetListMatrixTest.cs
+++ UnitTests/NetListMatrixTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using didactic_palm_tree.Simulation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StarMathLib;
 
 namespace UnitTests
 {
